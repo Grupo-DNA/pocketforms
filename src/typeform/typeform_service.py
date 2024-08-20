68c41ea40c73456f584651945eccca6741a102ad
otaviopl <otavio.pereira.lopes@gmail.com>
2024-08-20 10:49:52 -0300

    REFACTOR: new update forms fun behavior

--- src/typeform/typeform_service.py
+++ src/typeform/typeform_service.py
@@ -8,9 +8,17 @@ class TypeformService:
             'Authorization': f'Bearer {self.token}',
             'Content-Type': 'application/json'
         }
-
     def update_form(self, data):
         url = f'https://api.typeform.com/forms/{self.form_id}'
         response = requests.patch(url, json=data, headers=self.headers)
-        response.raise_for_status()
+        
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(f'HTTPError: {e}')
+            print(f'Status Code: {response.status_code}')
+            print(f'Response Body: {response.text}')
+            raise
+
         return response.json()
+
